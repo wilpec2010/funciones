fb97f5db1e9ac7cf48d3c1abcf2461891f50f063
Wilson Perez Correa <wilpec2017@gmail.com>
2022-06-29 21:08:13 -0500

    La primera funcion de pepito Septiembre

--- funciones.py
+++ funciones.py
@@ -14,3 +14,11 @@ def resta():
     c=a-b
     print("El resultado de la resta es: ",c)
 resta()
+
+def multi():
+    a=4
+    b=8
+    c=a*b
+    print("El resultado de la multiplicacion es: ",c)
+multi()
+    
