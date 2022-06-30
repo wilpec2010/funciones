aa449756bd68ebe25a08d922efcb8aff6ba2af30
Wilson Perez Correa <wilpec2017@gmail.com>
2022-06-29 20:39:44 -0500

    Mi tercera funcion, Agosto

--- funciones.py
+++ funciones.py
@@ -5,5 +5,12 @@ def nombre(): # mi primera función.
     a=3
     b=4
     c=a+b
-    print(c)
+    print("El resultado de la suma es: ",c)
 nombre() 
+
+def resta():
+    a=2
+    b=1
+    c=a-b
+    print("El resultado de la resta es: ",c)
+resta()
